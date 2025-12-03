4ff48b001823eb13488ffed8a2f243cd88b2400b
Frank Mousset <moussetfrank@gmail.com>
2025-12-03 23:20:00 +0100

    Small change to day3

--- day03.pl
+++ day03.pl
@@ -4,27 +4,27 @@
 :- use_module(library(pure_input), [phrase_from_stream/2]).
 :- use_module(library(apply), [maplist/3, foldl/4]).
 
-codes_digits(Codes, Digits) :-
-    maplist([X,Y]>>(Y is X-48), Codes, Digits).
-
 parse([Bank|Banks]) -->
-    digits(Codes),
+    digits(Digits),
     {
-        length(Codes, D), D > 1, codes_digits(Codes, Bank)
+        length(Digits, D), D > 1,
+        maplist([X,Y]>>(Y is X-48), Digits, Bank)
     },
     blanks,
     parse(Banks).
 parse([]) --> eos.
 
-% U is the joltage for a battery bank B, where exactly K batteries
-% can be turned on.
+% Main predicate: U is the joltage for a list of battery ratings, if
+% exactly K batteries can be turned on.
 joltage(Bank, K, U) :-
     reverse(Bank, BankRev),
     findall(I-B, nth0(I, BankRev, B), IndexedRev),
     reverse(IndexedRev, Indexed),
     joltage_(Indexed, K, U).
 
-joltage_(_, 0, 0).
+% Auxiliary predicate operating on a list of pairs I-B, where B is
+% the battery rating and I is the number of batteries coming after
+% the current battery.
 joltage_(Indexed, K, U) :-
     K > 0,
     foldl(maximum, Indexed, K-0-0, _-L-U0),
@@ -33,6 +33,7 @@ joltage_(Indexed, K, U) :-
     K1 is K-1,
     joltage_(Suffix, K1, U1),
     U is 10^(K-1)*U0+U1.
+joltage_(_, 0, 0).
 
 % Auxiliary predicate for the fold
 maximum(I-_, K-J-Max, K-J-Max) :- I+1 < K.
