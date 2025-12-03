20ec8f3c5644f27e0faaa963e97626dc1fdb936b
moussetf <moussetfrank@gmail.com>
2025-12-03 09:53:10 +0100

    Nicer day02.pl

--- day02.pl
+++ day02.pl
@@ -2,53 +2,45 @@
 :- use_module(library(lists)).
 :- use_module(library(pure_input), [phrase_from_stream/2]).
 
-parse([A-B|Ranges]) --> digits(A), "-", digits(B), ",", parse(Ranges).
-parse([A-B]) --> digits(A), "-", digits(B), blanks.
+parse([A-B|Ranges]) --> integer(A), "-", integer(B), ",", parse(Ranges).
+parse([A-B]) --> integer(A), "-", integer(B), blanks.
 
 part1 :-
     current_input(Stdin), phrase_from_stream(parse(Ranges), Stdin),
-    findall(X, invalid_number(Ranges, 2, X), Xs),
-    list_to_set(Xs, Set),
-    sum_list(Set, Sum),
+    setof(X, invalid_number(Ranges, 2, X), Xs),
+    sum_list(Xs, Sum),
     write(Sum), nl, halt.
 
 part2 :-
     current_input(Stdin), phrase_from_stream(parse(Ranges), Stdin),
-    findall(X, invalid_number(Ranges, _, X), Xs),
-    list_to_set(Xs, Set),
-    sum_list(Set, Sum),
+    setof(X, K^invalid_number(Ranges, K, X), Xs),
+    sum_list(Xs, Sum),
     write(Sum), nl, halt.
 
-% Either Codes splits perfectly into K pieces and Fst is the first
-% piece (as a number), or it doesn't, and Fst is the power of 10
-% that is as long as the first piece should be.
-first(Codes, K, Fst) :-
-    length(Codes, NCodes),
-    L is div(NCodes, K),
+% Either the decimal representation of N splits perfectly into K pieces and Fst
+% is the first piece (interpreted as a number), or it doesn't, and then Fst is
+% the power of 10 that is as long as the first piece should be.
+first(N, K, Fst) :-
+    Length is ceil(log10(N+1)),
+    PieceLen is div(Length, K),
     (
-        NCodes is K*L ->
-            prefix(FstCodes, Codes),
-            length(FstCodes, L),
-            number_codes(Fst, FstCodes)
-    ;   Fst is 10^L
+        Length is K*PieceLen -> Fst is div(N, 10^((K-1)*PieceLen))
+    ;   Fst is 10^PieceLen
     ).
 
-% Ls is the K-fold concatenation of L with itself.
-repeat(L, K, Ls) :- repeat_(L, K, Ls, []).
-repeat_(_, 0) --> [].
-repeat_(L, K) --> {K > 0, K1 is K-1}, L, repeat_(L, K1).
+% True if M is the number obtained by K-fold concatenation of N with itself.
+repeatN(N, K, M) :- repeatN_(N, K, M, 0).
+repeatN_(_, 0, Acc, Acc).
+repeatN_(N, K, M, Acc) :-
+    K > 0, L is K-1,
+    Acc0 is 10^ceil(log10(N+1))*Acc + N,
+    repeatN_(N, L, M, Acc0).
 
 % True if N is an invalid number (with multiplicity K) for one of the
 % given ranges.
 invalid_number(Ranges, K, N) :-
     member(A-B, Ranges),
-    length(B, MaxLen), between(2, MaxLen, K),
-    first(A, K, FstA),
-    first(B, K, FstB),
-    between(FstA, FstB, Piece),
-    number_codes(Piece, PieceCodes),
-    repeat(PieceCodes, K, Codes),
-    number_codes(N, Codes),
-    number_codes(NumA, A),
-    number_codes(NumB, B),
-    between(NumA, NumB, N).
+    KMax is ceil(log10(B+1)), between(2, KMax, K),
+    first(A, K, Lower), first(B, K, Upper), between(Lower, Upper, Piece),
+    repeatN(Piece, K, N),
+    between(A, B, N).
