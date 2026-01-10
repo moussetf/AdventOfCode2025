867cacd5c28e93d6222955200180a2f0f4310d73
Frank Mousset <moussetfrank@gmail.com>
2026-01-10 13:40:42 +0100

    Use DCGs for day 1.

--- day01.pl
+++ day01.pl
@@ -1,29 +1,34 @@
 :- use_module(library(dcg/basics)).
-:- use_module(library(apply), [foldl/4]).
+:- use_module(library(dcg/high_order)).
+:- use_module(library(clpfd)).
 :- use_module(library(pure_input), [phrase_from_stream/2]).
 
-parse([N|Ns]) --> "R", integer(N), blanks, parse(Ns).
-parse([N|Ns]) --> "L", integer(M), {N is -M}, blanks, parse(Ns).
-parse([]) --> eos.
+parse(Ns) --> sequence(move, Ns).
+move(N) --> "R", integer(N), blanks.
+move(N) --> "L", integer(M), {N is -M}, blanks.
 
-count_zeroes(Move, Pos-Zs, Pos1-Zs1) :-
-    Pos1 is mod(Pos + Move, 100),
-    (
-        Pos1 is 0 -> Zs1 is Zs+1
-    ;   Zs1 = Zs
-    ).
+count_zeroes(Pos, NumZeroes) -->
+    [Move],
+    { Pos1 is mod(Pos + Move, 100) },
+    { Pos1 is 0 -> NumZeroes #= NumZeroes1 + 1 ; NumZeroes = NumZeroes1 },
+    count_zeroes(Pos1, NumZeroes1).
+count_zeroes(_, 0) --> eos.
 
-count_crossings(Move, Pos-Zs, Pos1-Zs1) :-
-    Pos1 is mod(Pos + Move, 100),
-    (
-        Move >= 0 -> Zs1 is Zs + div(Pos + Move, 100)
-    ;   Zs1 is Zs + div(mod(100 - Pos, 100) - Move, 100)
-    ).
+count_crossings(Pos, NumCrossings) -->
+    [Move],
+    { Pos1 is mod(Pos + Move, 100) },
+    {
+        Move >= 0
+    ->  NumCrossings #= NumCrossings1 + div(Pos + Move, 100)
+    ;   NumCrossings #= NumCrossings1 + div(mod(100 - Pos, 100) - Move, 100)
+    },
+    count_crossings(Pos1, NumCrossings1).
+count_crossings(_, 0) --> eos.
 
 main :-
     current_input(Stdin),
     phrase_from_stream(parse(Ns), Stdin),
-    foldl(count_zeroes, Ns, 50-0, _-Part1),
-    foldl(count_crossings, Ns, 50-0, _-Part2),
+    phrase(count_zeroes(50, Part1), Ns),
+    phrase(count_crossings(50, Part2), Ns),
     format("~w~n~w~n", [Part1, Part2]),
     halt.
